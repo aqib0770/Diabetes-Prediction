d2eba26dd27746b7fda3f687c33548632e3b4e76
aqib0770 <aqibansari72a@gmail.com>
2023-08-11 02:36:52 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request,app
+import pickle
+
+application=Flask(__name__)
+app=application
+
+scaler=pickle.load(open('Model/standardScaler','rb'))
+model=pickle.load(open('Model/Log_reg','rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    result=""
+    if request.method=='POST':
+        
+        Pregnancies=int(request.form.get('Pregnancies'))
+        Glucose=float(request.form.get('Glucose'))
+        BloodPressure=float(request.form.get("BloodPressure"))
+        SkinThickness=float(request.form.get("SkinThickness"))
+        Insulin=float(request.form.get('Insulin'))
+        Bmi=float(request.form.get('Bmi'))
+        DiabetesPedigreeFunction=float(request.form.get('DiabetesPedigreeFunction'))
+        Age=int(request.form.get('Age'))
+
+        new_data=scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,
+                                    Insulin,Bmi,DiabetesPedigreeFunction,Age]])
+        prediction = model.predict(new_data)
+
+        if prediction[0]==1:
+            result='Diabetic'
+        else:
+            result='Non-Diabetic'
+
+        return render_template('home.html',results=result)
+    else:
+        return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+
